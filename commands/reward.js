e102a76d27a22e334f7b03a0813c2c0a7ad9750f
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 18:14:28 -0400

    Update reward.js

--- commands/reward.js
+++ commands/reward.js
@@ -3,19 +3,37 @@ let gems = require("../gems.json");
 
 module.exports.run = async (bot, message, args) => {
   if(message.author.id !== '284137818895417344') return message.reply("only my owner can use this command. (You didn't know logan was a slave did you?)");
-  let person = args[0];
-  let amt = args[1];
-  bot.users.filter(u => u.name === `${person}`).forEach(user => {
-    if(!gems[user.id]) {
-      gems[user.id] = {
-        gems: amt + 50,
-        tokens: 0
-      }
-      return;
-    }
-    gems[user.id].gems = gems[user.id].gems + amt 
-    message.channel.send(`I have given ${user} ${amt} gems.`);
+  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if (!pUser) return message.channel.send(`Please mention a user!`)
+
+  if(!gems[pUser.id]){
+    gems[pUser.id] = {
+      gems: 50,
+      tokens: 0
+    };
+  }
+  
+  if(isNaN(args[1])) return message.reply("please supply a number!");  
+
+  let pCoins = gems[pUser.id].gems;
+  let pBank = gems[pUser.id].tokens;
+
+  gems[pUser.id] = {
+    gems: pCoins + parseInt(args[1]),
+    tokens: pBank
+  };
+
+    let giveEmbed = new Discord.RichEmbed()
+    .setColor('#000000')
+    .setThumbnail("https://tse3.mm.bing.net/th?id=OIP.QbYo3zcLpLvozS5IyOsKSAHaId&pid=15.1&P=0&w=300&h=300")
+    .setDescription(`<@${message.author.id}> gave ${args[1]} gems to ${pUser}`);
+  
+  message.channel.send(giveEmbed);
+
+  fs.writeFile("./gems.json", JSON.stringify(gems), (err) => {
+    if(err) cosole.log(err)
   });
+
 }
 
 module.exports.help = {
