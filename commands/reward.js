045615cd639866e8d6298e627552ac6e46050c64
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 17:51:13 -0400

    Update reward.js

--- commands/reward.js
+++ commands/reward.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 let gems = require("../gems.json");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.author.id !== '346687165868015616') return message.reply("only my owner can use this command. (You didn't know logan was a slave did you?)");
+  if(message.author.id !== '284137818895417344') return message.reply("only my owner can use this command. (You didn't know logan was a slave did you?)");
   let person = parseInt(args[0]);
   let amt = args[1];
   message.channel.send(`I am sending <@${person}> gems in the amount of ${amt}.`);
