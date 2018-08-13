26a1bb48e61907a22be0e59ae327494d94577321
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 15:10:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ bot.on('guildCreate', guild => {
   bot.user.setGame(`/help`);
 });
 
+bot.on('guildMemberAdd', member => {
+  if(!gems[member.id]) {
+    gems[member.id] = {
+      gems: 50,
+      tokens: 0
+    }
+  }
+  gems[member.id] = gems[member.id] + 250
+  member.send("You have joined a bumped server. You just gained 250 gems. (If you are new to this bot, be sure to check it out in the guild you joined by typing `/help` in a channel!)")
+});
+
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
