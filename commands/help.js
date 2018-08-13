02de8736ed8b3719a5fedc92f63ffe46782c1b0f
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 17:28:32 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {  
-  message.channel.send(`**__Commands:__**\n**/bal* - shows you how many diamonds you have.\n**/spin** - spins a wheel at a chance for either an ad or gems.\n**/bump** - bump your server to the top of the bump list so people can see and join it for gems. Trust me, it works.\n\n\`+250 gems for joining a bumped server.\``);
+  message.channel.send(`**__Commands:__**\n**/bal** - shows you how many diamonds you have.\n**/spin** - spins a wheel at a chance for either an ad or gems.\n**/bump** - bump your server to the top of the bump list so people can see and join it for gems. Trust me, it works.\n**invite** - send a link to force me into a server.\n\n\`+250 gems for joining a bumped server.\``);
 }
 
 module.exports.help = {
