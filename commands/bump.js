30c8261e9c5b73127ff5c2adcd2ea81e197f93bf
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 14:38:04 -0400

    Update bump.js

--- commands/bump.js
+++ commands/bump.js
@@ -2,6 +2,17 @@ const Discord = require("discord.js");
 let gems = require("../gems.json");
 
 module.exports.run = async (bot, message, args) => {
+  if(!gems[message.author.id]) {
+    gems[message.author.id] = {
+      gems: 50,
+      tokens: 0
+    }
+  }
+  if (gems[message.author.id].gems < 2499) {
+	  message.channel.send(`Bumping your server costs 2500 gems! You only have ${gems[message.author.id].gems} gems currently. The fastest way to get gems is joining other bumped servers. (250 gems per join.)`);
+	  return;
+  }
+ gems[message.author.id].gems = gems[message.author.id].gems - 2500
 	if (message.author.id !== '346687165868015616') {
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No. Why would I do this for you? I have a **Admin only** policy.");
 	}
