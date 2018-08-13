0b20402f990d02690e4caab92a5102a0bf489c9e
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 18:00:01 -0400

    Update reward.js

--- commands/reward.js
+++ commands/reward.js
@@ -4,16 +4,18 @@ let gems = require("../gems.json");
 module.exports.run = async (bot, message, args) => {
   if(message.author.id !== '284137818895417344') return message.reply("only my owner can use this command. (You didn't know logan was a slave did you?)");
   let person = parseInt(args[0]);
-  let amt = args[1];
-  message.channel.send(`I am sending <@${person}> gems in the amount of ${amt}.`);
-  if(!gems[person]) {
-    gems[person] = {
-      gems: amt + 50,
-      tokens: 0
+  let amt = parseInt(args[1]);
+  bot.users.filter(u => u.id === `${person}`).forEach(user => {
+    if(!gems[user.id]) {
+      gems[user.id] = {
+        gems: amt + 50,
+        tokens: 0
+      }
+      return;
     }
-    return;
+    gems[user.id].gems = gems[user.id].gems + amt 
+    message.channel.send(`I have given ${user} ${amt} gems.`);
   }
- gems[person].gems = gems[person].gems + amt 
 }
 
 module.exports.help = {
