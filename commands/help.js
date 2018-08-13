f14f17db02136ec4f159eb4563e937a3bccdcaa1
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-13 15:03:45 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {  
+  message.channel.send(`**__Commands:__**\n**/bal* - shows you how many diamonds you have.\n**/spin** - spins a wheel at a chance for either an ad or gems.\n**/bump** - bump your server to the top of the bump list so people can see and join it for gems. Trust me, it works.\n\n\`+250 gems for joining a bumped server.\``);
+}
+
+module.exports.help = {
+  name: "help"
+}
