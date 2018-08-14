29fcb6f9779957a51fbbda1c6712b708e3147e45
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-14 19:45:26 -0400

    Create shop.js

---
+++ commands/shop.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+let gems = require("../gems.json");
+
+module.exports.run = async (bot, message, args) => {
+  let spinEmbed = new Discord.RichEmbed()
+  .setColor('#27ae60')
+  .setTitle(`Shop:`)
+  .setDescription("#1 - A frog emoji. ($50)\n#2 - Two frog emojis. ($80)\n#3 - Some tea. ($100)\n#4 COFFEE. ($120)\n\nTo buy something from the shop, do `$buy` and then the item number. Example: `$buy #3`");
+  message.channel.send(spinEmbed);
+}
+
+module.exports.help = {
+  name: "shop"
+}
