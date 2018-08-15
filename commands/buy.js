a0361680add266aa4d2993eed9f17357fa70c527
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-14 20:20:44 -0400

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -5,13 +5,24 @@ module.exports.run = async (bot, message, args) => {
   let item = args[0];
   if(!item) return message.reply("you have to supply an item code. Example: `$buy #4`");
   
+  if(!gems[message.author.id]) {
+    gems[message.author.id] = {
+      gems: 50,
+      tokens: 0
+    }
+  }
+  
   if (item === '#1') {
+    if(gems[message.author.id].gems < 50) return message.reply("you don't have enough money to buy this.");
     message.channel.send(`**${message.author.username},** you have purchased a frog emoji.\nHere is your purchase: 🐸`);
   } else if (item === '#2') {
+    if(gems[message.author.id].gems < 80) return message.reply("you don't have enough money to buy this.");
     message.channel.send(`**${message.author.username},** you have purchased two frog emojis.\nHere is your purchase: 🐸 - 🐸`);
   } else if (item === '#3') {
+    if(gems[message.author.id].gems < 100) return message.reply("you don't have enough money to buy this.");
     message.channel.send(`**${message.author.username},** you have purchased some tea.\nHere is your purchase: 🍵`);
   } else if (item === '#4') {
+    if(gems[message.author.id].gems < 120) return message.reply("you don't have enough money to buy this.");
     message.channel.send(`**${message.author.username},** you have purchased COFFEE.\nHere is your purchase: ☕`);
   } else {
     message.channel.send("The item code you chose doesn't exist.");
