84d0a4b7bdba5e80886be252045e52b16899cb58
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-08-15 21:00:16 -0400

    Create 55x2.js

---
+++ commands/55x2.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const gems = require("../gems.json");
+let mai = message.author.id;
+//h
+exports.run = async (bot, message, args) => {
+  if(!gems[message.author.id]) {
+    gems[message.author.id] = {
+      gems: 50,
+      tokens: 0
+    }
+  }
+  if(isNaN(args[0])) return message.reply("please supply a number!");
+  
+  if(gems[mai].gems < args[0]) return message.reply("you are too broke for that bet!");
+  if(parseInt(args[0]) < 1) return message.reply("you have to bet a number above $0!")
+  
+  let dieAmt = Math.floor(Math.random() * 99) + 1;
+
+  if (dieAmt > 54) {
+    gems[mai].gems = gems[mai].gems + parseInt(args[0])
+    message.channel.send(`**${message.author.username}:**\nYou have rolled a ${dieAmt} and won ${parseInt(args[0])}!`);
+  } else {
+    gems[mai].gems = gems[mai].gems - parseInt(args[0])
+    message.channel.send(`**${message.author.username}:**\nYou have rolled a ${dieAmt} which is lower than 55 so you loose ${parseInt(args[0])}!`);
+  }
+}
+
+module.exports.help = {
+  name: "55x2"
+}
