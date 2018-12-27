363c1eb9fc06bcb69ca66d76a4d21337a7363e84
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-12-27 18:49:49 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {  
-  message.channel.send(`Force me to join your dead server wih this link:\n\thttps://discordapp.com/oauth2/authorize?client_id=478337796520673311&permissions=8&scope=bot`);
+  message.channel.send(`Force me to join your server with this link:\n\thttps://discordapp.com/oauth2/authorize?client_id=527985231366455316&permissions=8&scope=bot`);
 }
 
 module.exports.help = {
