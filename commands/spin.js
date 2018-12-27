37c5f6292558796b9d94c9a297b69f518ebe68de
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-12-27 18:53:10 -0500

    Update spin.js

--- commands/spin.js
+++ commands/spin.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const gems = require("../gems.json");
 
-const claim_cooldown_time = 30;
+const claim_cooldown_time = 10;
 const claim_talked_users = new Set();
 //h
 exports.run = async (bot, message, args) => {
-  if (claim_talked_users.has(message.author.id)) return message.reply("you have to wait 30 seconds between spins.");
+  if (claim_talked_users.has(message.author.id)) return message.reply("you have to wait 10 minutes between spins.");
   if(!gems[message.author.id]) {
     gems[message.author.id] = {
       gems: 50,
@@ -18,38 +18,38 @@ exports.run = async (bot, message, args) => {
   let ppick = Math.floor((Math.random() * prize.length));  
   let amt = 0;
   if (prize[ppick] === "💰") {
-    gems[message.author.id].gems = gems[message.author.id].gems + 1000
+    gems[message.author.id].gems = gems[message.author.id].gems + 5000
     amt = amt + 1000
   }
   if (prize[ppick] === "💎") {
-    gems[message.author.id].gems = gems[message.author.id].gems + 180
+    gems[message.author.id].gems = gems[message.author.id].gems + 2000
     amt = amt + 180
   }
   if (prize[ppick] === "💸") {
-    gems[message.author.id].gems = gems[message.author.id].gems + 120
+    gems[message.author.id].gems = gems[message.author.id].gems + 1000
     amt = amt + 120
   }
   if (prize[ppick] === "🌸") {
-    gems[message.author.id].gems = gems[message.author.id].gems + 80
+    gems[message.author.id].gems = gems[message.author.id].gems + 500
     amt = amt + 100
   }
   if (prize[ppick] === "🍎") {
-    gems[message.author.id].gems = gems[message.author.id].gems + 35
+    gems[message.author.id].gems = gems[message.author.id].gems + 100
     amt = amt + 35
   }
   if (prize[ppick] === "🥔") {
-    gems[message.author.id].gems = gems[message.author.id].gems + 20
+    gems[message.author.id].gems = gems[message.author.id].gems + 50
     amt = amt + 20
   }
   let spinEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
   .setTitle(`${message.author.username}:`)
-  .setDescription(`Roll: ${prize[ppick]} = $${amt}\nGiven By: ${ad[adpick]}\n\nRoll Chances:\n🥔 -32%\t🍎 - 28%\n🌸 - 20%\t💸 - 10%\n💎- 8%\t💰 - 2%`);
+  .setDescription(`Roll: ${prize[ppick]} = $${amt}\n\nRoll Chances:\n🥔 -32%\t🍎 - 28%\n🌸 - 20%\t💸 - 10%\n💎- 8%\t💰 - 2%`);
   message.channel.send(spinEmbed);
   claim_talked_users.add(message.author.id);
     setTimeout(() => {
       claim_talked_users.delete(message.author.id);
-    }, claim_cooldown_time * 1000);
+    }, claim_cooldown_time * 60000);
 }
 
 module.exports.help = {
