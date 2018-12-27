4751f3fc1a5b4ba21c7aa913bb828fbc0e0c348f
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-12-27 18:46:59 -0500

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   let spinEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
   .setTitle(`Shop:`)
-  .setDescription("#1 - A frog emoji. ($50)\n#2 - Two frog emojis. ($80)\n#3 - Some tea. ($100)\n#4 - COFFEE. ($120)\n#5 - a DM to HalfBaked. ($500)\n\nTo buy something from the shop, do `$buy` and then the item number.\nExample: `$buy #3`");
+  .setDescription("#1 - A frog emoji. ($50)\n#2 - Two frog emojis. ($80)\n#3 - Some tea. ($100)\n#4 - COFFEE. ($120)\n#5 - a DM to HalfBakedGaming. ($5000)\n\nTo buy something from the shop, do `$buy` and then the item number.\nExample: `$buy #3`");
   message.channel.send(spinEmbed);
 }
 
