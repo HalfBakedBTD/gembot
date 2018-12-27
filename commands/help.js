94986829d7c3b27eda2d35123badb6dd437307a2
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-12-27 18:41:39 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {  
-  message.channel.send(`**__Commands:__**\n**$help** - only people with 60 IQ wouldn't know what this is...\n**$55x2** - bet for a chance to double your bet!\n**$75x3** - bet at a chance to triple your bet!\n**$shop** - shows items to buy.\n**$buy** - lets you buy an item from the shop.\n**$bal** - shows you how much moneys you have.\n**$spin** - spins a wheel at a chance for moneys.\n**$bump** - bump your server to the top of the bump list.\n**$invite** - send a link to force me into a server.\n**$flip** - flip a coin...\n\n**__Beta Commands:__**\n**$stockMarket** - shows the stock market.\n**$myStock** - lets you view your stock.\n**$buyStock** - lets you buy stock.\n**$sellStock** - lets you sell stock.\n\n\`+$250 for joining a bumped server.\`\n\`+$500 for joining https://discord.gg/4XPxu73\``);
+  message.channel.send(`**__Commands:__**\n**$help** - only people with 60 IQ wouldn't know what this is...\n**$55x2** - bet for a chance to double your bet!\n**$75x3** - bet at a chance to triple your bet!\n**$shop** - shows items to buy.\n**$buy** - lets you buy an item from the shop.\n**$bal** - shows you how much moneys you have.\n**$spin** - spins a wheel at a chance for moneys.\n**$invite** - send a link to force me into a server.\n**$flip** - flip a coin...\n\n**__Beta Commands:__**\n**$stockMarket** - shows the stock market.\n**$myStock** - lets you view your stock.\n**$buyStock** - lets you buy stock.\n**$sellStock** - lets you sell stock.\n\n`);
 }
 
 module.exports.help = {
