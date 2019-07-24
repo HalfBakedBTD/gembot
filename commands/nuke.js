c9acc18b3ed6a33545fc6f5e2d55c489850e6b15
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-07-23 23:02:48 -0400

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -4,8 +4,7 @@ let gems = require("../gems.json");
 module.exports.run = async (bot, message, args) => {
   message.guild.members.filter(u => u.id ==! 'stock-prices').forEach(user => {
     let reason = "RAID!";
-    await user.ban(reason)
-      .catch(error => message.reply(`Sorry, I couldn't ban ${user} because of : ${error}`));
+    user.ban(reason)
     message.reply(`${member.user.username} has been banned by ${message.author.username} because: **__${reason}__**`);
   });
 }
